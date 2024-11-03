edc9bed8b99d179ba77a7017bb95b490d4474778
JakeRSchwartz <jakerschwartz47@gmail.com>
2024-11-03 15:51:09 -0600

    comments

--- RouteFinder.py
+++ RouteFinder.py
@@ -6,6 +6,9 @@ from classes.Helpers import Helpers
 def main():
     #Get Graph
     #G = ox.graph_from_place("Manhattan, New York, USA", network_type="drive")
+    
+    #ox.save
+    #ox.save_graphml(G, "./data/manhattan_drive.graphml")
 
     # Load the graph from a GraphML file
     G = ox.load_graphml("./data/manhattan_drive.graphml")
@@ -33,14 +36,16 @@ def main():
     astar_path = route_algo.a_star(start, goal)
     print("A* Path:", astar_path)
     if astar_path:
-        print("A* Path length:", sum(graph[astar_path[i]][astar_path[i + 1]] for i in range(len(astar_path) - 1)))
+        astar_length_miles = sum(graph[astar_path[i]][astar_path[i + 1]] for i in range(len(astar_path) - 1)) / 1609.34
+        print(f"A* Path length (Miles): {astar_length_miles:.2f}")
         ox.plot_graph_route(G, astar_path, route_linewidth=2, node_size=0)
 
     # Run Dijkstra algorithm
     dijkstra_path = route_algo.dijkstra(start, goal)
     print("Dijkstra Path:", dijkstra_path)
     if dijkstra_path:
-        print("Dijkstra Path length:", sum(graph[dijkstra_path[i]][dijkstra_path[i + 1]] for i in range(len(dijkstra_path) - 1)))
+        dijkstra_length_miles = sum(graph[dijkstra_path[i]][dijkstra_path[i + 1]] for i in range(len(dijkstra_path) - 1)) / 1609.34
+        print(f"Dijkstra Path length (Miles): {dijkstra_length_miles:.2f}")
         ox.plot_graph_route(G, dijkstra_path, route_linewidth=2, node_size=0)
 
 
